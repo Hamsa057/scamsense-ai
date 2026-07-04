e755f3421fe38192c7e1708fa874ef8f039fc470
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2026-07-04 23:10:46 +0530

    add commit

---
+++ frontend/index.js
@@ -0,0 +1,90 @@
+const analyzeBtn = document.getElementById("analyze-btn");
+const messageInput = document.getElementById("message-input");
+const resultCard = document.getElementById("result-card");
+
+const score = document.getElementById("score");
+const verdict = document.getElementById("verdict");
+const reasonsList = document.getElementById("reasons-list");
+const errorMessage = document.getElementById("error-message");
+
+analyzeBtn.addEventListener("click", async function () {
+    const message = messageInput.value.trim();
+
+    if (message === "") {
+        errorMessage.innerText =
+            "Please paste an SMS or email first.";
+        return;
+    }
+
+    errorMessage.innerText = "";
+
+    analyzeBtn.innerText = "Analyzing...";
+    analyzeBtn.disabled = true;
+
+    try {
+        const response = await fetch(
+            "http://localhost:5000/analyze",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    message: message
+                })
+            }
+        );
+
+        let data = await response.text();
+
+        data = data
+            .replace(/```json/g, "")
+            .replace(/```/g, "")
+            .trim();
+
+        data = JSON.parse(data);
+
+        resultCard.hidden = false;
+
+        score.innerText = `${data.score}/100`;
+        verdict.innerText = data.verdict;
+
+        let html = "";
+
+        for (let reason of data.reasons) {
+            html += `<li>${reason}</li>`;
+        }
+
+        reasonsList.innerHTML = html;
+
+        // Score Color
+        if (data.score >= 70) {
+            score.style.color = "#ef4444";
+        }
+        else if (data.score >= 30) {
+            score.style.color = "#f59e0b";
+        }
+        else {
+            score.style.color = "#22c55e";
+        }
+
+        // Verdict Color
+        if (data.verdict === "Likely Scam") {
+            verdict.style.color = "#ef4444";
+        }
+        else if (data.verdict === "Suspicious") {
+            verdict.style.color = "#f59e0b";
+        }
+        else {
+            verdict.style.color = "#22c55e";
+        }
+    }
+    catch (err) {
+        console.log(err);
+        alert("Something went wrong. Please try again.");
+    }
+    finally {
+        analyzeBtn.innerText = "Analyze Message";
+        analyzeBtn.disabled = false;
+    }
+});
